dd79472ea91e7407b4f9803e84b3cf48ebc52535
Vishal <Vishal@admin-2.local>
2014-03-05 10:27:25 +0530

    Changed a migration

--- db/migrate/20140301092558_add_topic_id_voters.rb
+++ db/migrate/20140301092558_add_topic_id_voters.rb
@@ -1,11 +1,15 @@
 class AddTopicIdVoters < ActiveRecord::Migration
 
   def self.up
+    create_table :voters do |t|
+      t.timestamps
+    end
     add_column :voters, :topic_id, :integer
   end
 
   def self.down
     remove_column :voters, :topic_id
+    drop_table :voters
   end
 
 end
