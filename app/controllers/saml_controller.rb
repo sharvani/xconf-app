ddb5e0caf2912abfa3c82fa18ed78bb6ba161087
Sharvani <sharvani.n.s@thoughtworks.com>
2014-10-29 18:15:40 +0530

    Fixing inspecting saml response object

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -5,7 +5,7 @@ class SamlController < ApplicationController
 
   def auth
     response = Onelogin::Saml::Response.new(Base64.decode64(params[:SAMLResponse]))
-    Rails.logger.error response
+    Rails.logger.error response.inspect
     session[:user_id] = "SET"
     redirect_to params[:RelayState] || '/'
   end
