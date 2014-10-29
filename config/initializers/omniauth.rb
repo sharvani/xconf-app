c3f2b9ef474048e4ddf9b3ecf550a9fc7234580e
Sharvani <sharvani.n.s@thoughtworks.com>
2014-10-29 17:34:24 +0530

    Using env instead of config files and Skipping CSRF protection for Okta callback.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,14 +2,14 @@ require 'omniauth'
 require 'omniauth-saml'
 require 'yaml'
 
-OKTA_CONFIGS = YAML.load(File.read(File.join(Rails.application.config.root,"config", "okta.yml")))
+#OKTA_CONFIGS = YAML.load(File.read(File.join(Rails.application.config.root,"config", "okta.yml")))
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
     :issuer                             => "https://enigmatic-beach-7933.herokuapp.com/topics",
-    :idp_sso_target_url                 => OKTA_CONFIGS["idp_sso_target_url"],
+    :idp_sso_target_url                 => ENV["idp_sso_target_url"],
     :idp_sso_target_url_runtime_params  => {:redirectUrl => :RelayState},
-    :idp_cert_fingerprint               => OKTA_CONFIGS["idt_cert_fingerprint"],
+    :idp_cert_fingerprint               => ENV["idt_cert_fingerprint"],
     :name_identifier_format             => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 end
