d237664d3a6370b21ca6fe48d7b67504474a4e2c
Sharvani <sharvani.n.s@thoughtworks.com>
2014-10-29 18:11:26 +0530

    Adding proper logging

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -4,7 +4,7 @@ class SamlController < ApplicationController
 
   def auth
     response = Onelogin::Saml::Response.new(Base64.decode64(params[:SAMLResponse]))
-    Rails.log response
+    Rails.logger.error response
     session[:user_id] = "SET"
     redirect_to params[:RelayState] || '/'
   end
