bd10c1458c718d771beed1d2b0c52e1642b09600
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2014-10-30 02:36:43 +0530

    Added parameter filtering and redirect filtering

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module XconfApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.filter_parameters << :SAMLResponse
+    config.filter_redirect << /thougtworks\.okta(preview)?.com/
   end
 end
